5c00e6de797131acf5782824c1c497154034d19d
MaundyMoo <maund007@gmail.com>
2019-10-12 20:30:25 +0100

    table to show output

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from datetime import datetime 
 
 app = Flask(__name__)
 
@@ -10,7 +11,10 @@ def index():
 def address():
 	postcode = request.form['postcode']
 	print(postcode)
-	return redirect('/')
+	# TODO input data into scraper methods and return data
+	data = [['Tuesday', '01', 'General'], ['Tuesday', '08', 'General'], ['Thursday', '10', 'Recycling'], ['Thursday', '10', 'Glass'], ['Tuesday', '15', 'General'], ['Tuesday', '22', 'General'], ['Thursday', '24', 'Recycling'], ['Thursday', '24', 'Glass'], ['Tuesday', '29', 'General']]
+
+	return render_template("index.html", data=data, month=datetime.now().strftime('%b'))
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0') 

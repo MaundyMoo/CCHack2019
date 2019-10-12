b1958d4983335b0f81c31b7d42895710ae78f069
MaundyMoo <maund007@gmail.com>
2019-10-12 15:53:44 +0100

    Added html form

--- webapp.py
+++ webapp.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/address', methods = ['POST'])
+def address():
+	postcode = request.form['postcode']
+	print(postcode)
+	return redirect('/')
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0') 
